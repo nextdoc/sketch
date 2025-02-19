a54ea9d06eb02004f75cbca928a3af85083f794b
Steve <steve.buikhuizen@gmail.com>
2025-02-19 18:28:15 +1100

    FEATURE Add more steps to the Happy Path example.

--- examples/mobile_weather_app/happy_path_test.clj
+++ examples/mobile_weather_app/happy_path_test.clj
@@ -52,17 +52,146 @@
                          :direction :response
                          :payload   user}]}))})
 
-; TODO app loads location, requests weather
-; TODO api tracks user location. evil!
-; TODO lambda calls openweatherapi
-; TODO lambda pushes update
+(defn app-weather-request []
+  {:actor   :iphone/weather-app
+   :action  "app uses CLocationManager to get lat/long and requests weather"
+   :handler (fn [{:keys [state fixtures]}]
+              (let [{:keys [user-name]} (first (sketch-run/query (:core-data state) :users (constantly true)))]
+                {:emit [{:to      :aws/lambda
+                         :request :weather-info
+                         :payload {:user-name user-name
+                                   :latitude  (:lat fixtures)
+                                   :longitude (:long fixtures)}}]}))})
+
+(defn lambda-weather-start []
+  {:actor   :aws/lambda
+   :action  "AWS tracks (evil) user location and requests weather from provider"
+   :handler (fn [{:keys [state messages]}]
+              (let [{:keys [user-name latitude longitude]} (-> messages last :message :payload)
+                    users-found (sketch-run/query (:ddb state) :users (comp #{user-name} :user-name))]
+                (is (= 1 (count users-found)))
+                (sketch-run/put-record! (:ddb state) :users
+                                        (:id (first users-found))
+                                        (merge (first users-found)
+                                               {:latitude  latitude
+                                                :longitude longitude}))
+                {:emit [{:to      :open-weather/api
+                         :request :one-call
+                         :payload {:app-id (sketch-run/get-value (:env state) "API_KEY")
+                                   :lat    latitude
+                                   :lon    longitude}}]}))})
+
+(defn api-weather-response []
+  {:actor   :open-weather/api
+   :action  "OpenWeather API returns current weather and forecast"
+   :handler (fn [{:keys [fixtures]}]
+              ; Ignoring app id validation and input lat/long
+              {:emit [{:to        :aws/lambda
+                       :request   :one-call
+                       :direction :response
+                       :payload   (-> fixtures :locations :sydney)}]})})
+
+(defn lambda-weather-response []
+  {:actor   :aws/lambda
+   :action  "AWS Lambda forwards weather data to mobile app"
+   :handler (fn [{:keys [messages]}]
+              (let [weather-data (-> messages last :message :payload)]
+                {:emit [{:to        :iphone/weather-app
+                         :request   :weather-info
+                         :direction :response
+                         :payload   {:name       (:timezone weather-data)
+                                     :temp       (-> weather-data :current :temp)
+                                     :feels-like (-> weather-data :current :feels-like)}}]}))})
+
+(defn app-weather-response []
+  {:actor   :iphone/weather-app
+   :action  "Mobile app stores weather data in Core Data"
+   :handler (fn [{:keys [state messages]}]
+              (let [weather-data (-> messages last :message :payload)]
+                (sketch-run/put-record! (:core-data state) :cities (random-uuid) weather-data)
+                {:emit []}))})
+
+(defn lambda-poll-weather []
+  {:actor   :aws/lambda
+   :action  "AWS Lambda polls weather for tracked locations"
+   :handler (fn [{:keys [state]}]
+              (let [users (sketch-run/query (:ddb state) :users (comp some? :latitude))]
+                {:emit (for [user users]
+                         {:to      :open-weather/api
+                          :request :one-call
+                          :payload {:app-id (sketch-run/get-value (:env state) "API_KEY")
+                                    :lat    (:latitude user)
+                                    :lon    (:longitude user)}})}))})
+
+(defn api-weather-alert-response []
+  {:actor   :open-weather/api
+   :action  "OpenWeather API returns weather with storm alert"
+   :handler (fn [{:keys [fixtures]}]
+              (let [alert {:sender-name "Bureau of Meteorology"
+                           :event       "Severe Thunderstorm Warning"
+                           :start       1684929490
+                           :end         1684977332
+                           :description "Severe thunderstorms likely to produce damaging winds and heavy rainfall"
+                           :tags        ["Thunderstorm" "Rain" "Wind"]}]
+                {:emit [{:to        :aws/lambda
+                         :request   :one-call
+                         :direction :response
+                         :payload   (-> fixtures :locations :sydney
+                                        (assoc :alerts [alert]))}]}))})
+
+(defn lambda-push-weather-alert []
+  {:actor   :aws/lambda
+   :action  "AWS Lambda pushes weather alert to mobile app"
+   :handler (fn [{:keys [messages]}]
+              (let [weather-data (-> messages last :message :payload)]
+                {:emit [{:to      :iphone/weather-app
+                         :event   :weather-change
+                         :payload {:name       (:timezone weather-data)
+                                   :temp       (-> weather-data :current :temp)
+                                   :feels-like (-> weather-data :current :feels-like)
+                                   :alerts     (mapv :event (:alerts weather-data))}}]}))})
+
+(defn app-weather-change []
+  {:actor   :iphone/weather-app
+   :action  "Mobile app updates weather data with alerts in Core Data"
+   :handler (fn [{:keys [state messages]}]
+              (let [weather-data (-> messages last :message :payload)
+                    cities (sketch-run/query (:core-data state) :cities #(= (:name %) (:name weather-data)))
+                    city (first cities)]
+                (when city
+                  (sketch-run/put-record! (:core-data state) :cities (:id city) weather-data))
+                {:emit []}))})
 
 ;;;; TEST UTILS ;;;;
 
 (defn initial-fixtures
   "provide text data to specific agents"
   []
-  {:iphone {:user-name "Rich"}})
+  {:iphone       {:user-name "Rich"
+                  :lat       -33.890897
+                  :long      151.276801}
+   :open-weather {:locations {:sydney {:lat             -33.890897
+                                       :lon             151.276801
+                                       :timezone        "Australia/Sydney"
+                                       :timezone-offset 36000
+                                       :current         {:dt         1684929490
+                                                         :sunrise    1684926645
+                                                         :sunset     1684977332
+                                                         :temp       295.15
+                                                         :feels-like 295.87
+                                                         :pressure   1012
+                                                         :humidity   72
+                                                         :dew-point  289.82
+                                                         :uvi        5.2
+                                                         :clouds     20
+                                                         :visibility 10000
+                                                         :wind-speed 5.7
+                                                         :wind-deg   135
+                                                         :wind-gust  8.2
+                                                         :weather    [{:id          800
+                                                                       :main        "Clear"
+                                                                       :description "clear sky"
+                                                                       :icon        "01d"}]}}}}})
 
 (defn reset-system! []
   {:reset (fn [system] (swap! system assoc :fixtures (initial-fixtures)))})
@@ -74,13 +203,22 @@
             :registry              (model/registry)
             :state-schemas-ignored #{}}))
 
-(def app-start-steps '[app-user-info-request
-                       lambda-cold-start
-                       api-user-info-response])
+(def app-start-chapter '[app-user-info-request
+                         lambda-cold-start
+                         api-user-info-response
+                         app-weather-request
+                         lambda-weather-start
+                         api-weather-response
+                         lambda-weather-response
+                         app-weather-response
+                         lambda-poll-weather
+                         api-weather-alert-response
+                         lambda-push-weather-alert
+                         app-weather-change])
 
 ; step thunks with indirection so exceptions can provide location
 (def test-steps (mapv #(ns-resolve *ns* %)
-                      (concat ['reset-system!] app-start-steps)))
+                      (concat ['reset-system!] app-start-chapter)))
 
 (deftest happy-path
   (->> {:steps          test-steps
--- examples/mobile_weather_app/weather_registry.cljc
+++ examples/mobile_weather_app/weather_registry.cljc
@@ -15,12 +15,21 @@
                                         [:type [:= :user-info]]
                                         [:user-name :string]]
    :aws-lambda/user-info-response      :weather/user
+   :aws-lambda/weather-info            [:map]
+   :aws-lambda/weather-info-request    [:map
+                                        [:user-name :string]]
+   :aws-lambda/weather-info-response   :weather/city
    :iphone/weather-app                 [:map]
    :iphone-core-data/city              :weather/city
    :iphone-core-data/user              [:map]
    :iphone-weather-app/core-data       [:map]
    :iphone-weather-app/error           [:map]
-   :iphone-weather-app/forecast-change [:map]})
+   :iphone-weather-app/weather-change  [:map]
+   :open-weather/api                   [:map]
+   :open-weather-api/error             [:map]
+   :open-weather-api/one-call          [:map]
+   :open-weather-api/one-call-request  :open-weather/onecall-request
+   :open-weather-api/one-call-response :open-weather/onecall-response})
 
 (def domain-schema
   {:weather/user [:map
@@ -28,12 +37,116 @@
                   [:status [:enum :active :blocked]]]
    :weather/city [:map
                   [:name :string]
-                  [:latitude :int]
-                  [:longitude :int]]
+                  [:temp :double]
+                  [:feels-like :double]
+                  [:alerts {:optional true} [:vector :string]]]})
+
+(def ^{:doc "Rest API https://openweathermap.org/api/one-call-3"}
+  open-weather-schema
+  ; note: this schema could be generated from published swagger schemas
+  ; created using AI from sample for demo purposes
+  {:open-weather/weather-condition [:map
+                                    [:id :int]
+                                    [:main :string]
+                                    [:description :string]
+                                    [:icon :string]]
+
+   :open-weather/precipitation     [:map
+                                    [:1h :double]]
+
+   :open-weather/wind-data         [:map
+                                    [:wind-speed :double]
+                                    [:wind-deg :int]
+                                    [:wind-gust {:optional true} :double]]
+
+   :open-weather/base-weather      [:map
+                                    [:dt :int]
+                                    [:temp :double]
+                                    [:feels-like :double]
+                                    [:pressure :int]
+                                    [:humidity :int]
+                                    [:dew-point :double]
+                                    [:uvi :double]
+                                    [:clouds :int]
+                                    [:visibility :int]
+                                    [:ref {:optional true} :open-weather/wind-data]
+                                    [:weather [:vector :open-weather/weather-condition]]
+                                    [:rain {:optional true} :open-weather/precipitation]
+                                    [:snow {:optional true} :open-weather/precipitation]]
+
+   :open-weather/daily-temp        [:map
+                                    [:day :double]
+                                    [:min :double]
+                                    [:max :double]
+                                    [:night :double]
+                                    [:eve :double]
+                                    [:morn :double]]
+
+   :open-weather/daily-feels-like  [:map
+                                    [:day :double]
+                                    [:night :double]
+                                    [:eve :double]
+                                    [:morn :double]]
+
+   :open-weather/alert             [:map
+                                    [:sender-name :string]
+                                    [:event :string]
+                                    [:start :int]
+                                    [:end :int]
+                                    [:description :string]
+                                    [:tags [:vector :string]]]
+
+   :open-weather/onecall-request   [:map
+                                    [:app-id :string]
+                                    [:lat :float]
+                                    [:lon :float]]
+
+   :open-weather/onecall-response  [:map
+                                    [:lat :double]
+                                    [:lon :double]
+                                    [:timezone :string]
+                                    [:timezone-offset :int]
+                                    [:current [:merge
+                                               :open-weather/base-weather
+                                               [:map
+                                                [:sunrise :int]
+                                                [:sunset :int]]]]
+                                    [:minutely {:optional true}
+                                     [:vector [:map
+                                               [:dt :int]
+                                               [:precipitation :double]]]]
+                                    [:hourly {:optional true}
+                                     [:vector [:merge
+                                               :open-weather/base-weather
+                                               [:map
+                                                [:pop :double]]]]]
+                                    [:daily {:optional true}
+                                     [:vector [:map
+                                               [:dt :int]
+                                               [:sunrise :int]
+                                               [:sunset :int]
+                                               [:moonrise :int]
+                                               [:moonset :int]
+                                               [:moon-phase :double]
+                                               [:summary :string]
+                                               [:temp :open-weather/daily-temp]
+                                               [:feels-like :open-weather/daily-feels-like]
+                                               [:pressure :int]
+                                               [:humidity :int]
+                                               [:dew-point :double]
+                                               [:ref :open-weather/wind-data]
+                                               [:weather [:vector :open-weather/weather-condition]]
+                                               [:clouds :int]
+                                               [:pop :double]
+                                               [:rain {:optional true} :double]
+                                               [:snow {:optional true} :double]
+                                               [:uvi :double]]]]
+                                    [:alerts {:optional true}
+                                     [:vector :open-weather/alert]]]
 
    })
 
 (defn registry []
-  (merge generated domain-schema
+  (merge generated domain-schema open-weather-schema
          (m/default-schemas)
          (mu/schemas)))
