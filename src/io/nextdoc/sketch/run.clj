c624636c39e1b1beadf9666ece8f20581ba893b8
Steve <steve.buikhuizen@gmail.com>
2025-02-23 15:51:26 +1100

    CHORE Release 21

--- src/io/nextdoc/sketch/run.clj
+++ src/io/nextdoc/sketch/run.clj
@@ -308,7 +308,7 @@
                                        :states  states
                                        :model   model-parsed
                                        :dev?    dev?
-                                       :tag     "r0.1.20"}
+                                       :tag     "r0.1.21"}
                                       (sequence-diagram-page)
                                       (html)))
     (log/info success-string)))
